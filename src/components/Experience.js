731678b3e9c75ab68b6e367f1ad8150e2b86123a
ruthvikm <ruthviknathchowdary@gmail.com>
2024-09-04 12:34:54 -0500

    updated experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,7 +9,7 @@ const Experience = () => {
                 <div className="job">
                     <h3>Research Assistant</h3>
                     <h4>Open-Source SLU, Saint Louis University, St. Louis</h4>
-                    <span>June 2023 - May 2024</span>
+                    <span>June 2023 - Present</span>
                     <p>
                         Tech-Lead for a team of four, spearheading the development of BubbleScan-AI, an open-source AI tool that automates scantron sheet analysis, improving grading efficiency and reducing manual effort by 95%. Also directed the Pi4Micronaut project, building a Java library for Raspberry Pi hardware integration using the Micronaut framework and Pi4J. Employed Agile methodologies to ensure high-quality software, timely delivery, and seamless team collaboration.
                     </p>
